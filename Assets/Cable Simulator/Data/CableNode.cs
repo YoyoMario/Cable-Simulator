77dfcb21a20b650e4a2be9473bed95759dd0e75b
YoyoMario <mario.haberle@gmail.com>
2024-06-19 22:44:49 +0200

    Rope functions well.

---
+++ Assets/Cable Simulator/Data/CableNode.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class CableNode
+{
+    public Vector3 OldPosition;
+    public Vector3 CurrentPosition;
+    public Vector3 PredictedPosition;
+    public Vector3 Acceleration;
+    public Vector3 Velocity;
+
+    public CableNode(Vector3 startPosition)
+    {
+        CurrentPosition = startPosition;
+        OldPosition = CurrentPosition;
+        PredictedPosition = CurrentPosition;
+        Acceleration = Vector3.zero;
+        Velocity = Vector3.zero;
+    }
+}
---
+++ Assets/Cable Simulator/Data/CableSolver.cs
@@ -0,0 +1,129 @@
+using UnityEngine;
+
+public class CableSolver : MonoBehaviour
+{
+    [Header("Settings:")]
+    public int SolverCount = 10;
+    public int NodeCount = 40;
+    [Range(0, 0.2f)] public float NodeDistance = 0.1f;
+    public float Gravity = 9.81f;
+    [Range(0.01f, 0.99f)] public float Elasticity = 1;
+    [Range(0.9f, 1.0f)] public float Dampening = 1;
+
+    public bool EndHandle = default;
+    public Transform EndHanlePoint = default;
+
+    [Header("Runtime Info:")]
+    [SerializeField] private CableNode[] _cableNodes = default;
+
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+
+        for (int i = 0; i < NodeCount - 1; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Gizmos.color = Color.green;
+            }
+            else
+            {
+                Gizmos.color = Color.white;
+            }
+            Gizmos.DrawLine(_cableNodes[i].CurrentPosition, _cableNodes[i + 1].CurrentPosition);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(_cableNodes[i].CurrentPosition, 0.025f);
+
+            //Vector3 direction = nodes[i].position - nodes[i+1].position; // Towards node 1.
+            //Gizmos.color = Color.yellow;
+            //DrawArrow.ForGizmo(nodes[i].position, direction, 0.05f, 20f);
+        }
+        Gizmos.DrawSphere(_cableNodes[NodeCount - 1].CurrentPosition, 0.025f);
+    }
+
+    private void Start()
+    {
+        InitializeCable();
+    }
+
+    private void Update()
+    {
+        // Calculate gravity velocity.
+        for (int i = 0; i < NodeCount; i++)
+        {
+            CableNode cableNode = _cableNodes[i];
+            cableNode.Velocity = Vector3.down * Gravity * Time.deltaTime;
+        }
+
+        // Gravity pass + main rope pass.
+        for (int i = 0; i < NodeCount; i++)
+        {
+            CableNode cableNode = _cableNodes[i];
+            Vector3 currentPosition = cableNode.CurrentPosition;
+            cableNode.CurrentPosition += ((cableNode.CurrentPosition - cableNode.OldPosition) * Dampening) + (cableNode.Velocity * Time.deltaTime);
+            cableNode.OldPosition = currentPosition;
+        }
+
+        // Solver.
+        for (int z = 0; z < SolverCount; z++)
+        {
+            ResolveNodePositions();
+        }
+    }
+
+    private void InitializeCable()
+    {
+        _cableNodes = new CableNode[NodeCount];
+        for (int i = 0; i < NodeCount; i++)
+        {
+            Vector3 nodeStartPosition = transform.position + (Vector3.down * NodeDistance * i);
+            _cableNodes[i] = new CableNode(nodeStartPosition);
+        }
+    }
+
+    private void ResolveNodePositions()
+    {
+        for (int i = 0; i < NodeCount - 1; i++)
+        {
+            CableNode node1 = _cableNodes[i];
+            CableNode node2 = _cableNodes[i + 1];
+
+            // Special case.
+            if (i == 0)
+            {
+                node1.CurrentPosition = transform.position;
+            }
+            if (EndHandle && i == NodeCount - 2)
+            {
+                node2.CurrentPosition = EndHanlePoint.position;
+            }
+
+
+            Vector3 direction = node1.CurrentPosition - node2.CurrentPosition; // Towards Node 1.
+            float distance = Vector3.Distance(node1.CurrentPosition, node2.CurrentPosition);
+
+            float pushPullStrength = default;
+            if (distance > 0)
+            {
+                pushPullStrength = (NodeDistance - distance) / distance;
+            }
+
+            Vector3 neededTranslation = direction * Elasticity * pushPullStrength;
+            node1.CurrentPosition += neededTranslation;
+            node2.CurrentPosition -= neededTranslation;
+
+            if (i == 0)
+            {
+                node1.CurrentPosition = transform.position;
+            }
+            if (EndHandle && i == NodeCount - 2)
+            {
+                node2.CurrentPosition = EndHanlePoint.position;
+            }
+        }
+    }
+}
